d57833420aecbdb463030ace4ac6e02f3df51c27
MatAviez05 <baezavilamateo@gmail.com>
2025-11-11 23:18:52 -0300

    cree el turnos service, para tests

---
+++ src/backend/src/service/TurnoService.ts
@@ -0,0 +1,36 @@
+import { Turno } from "../factories/Turno";
+
+export class TurnoService {
+
+    private turnosList:Turno[] = []
+    private id_autoincremental:number = 1
+
+    constructor(){}
+
+    getSize():number{
+        return this.turnosList.length
+    }
+
+    getTurnoList(): Turno[]{
+        throw new Error('Implemtar funcion')
+    }
+
+    getTurnoById(): Turno | undefined{
+        throw new Error('Implemtar funcion')
+    }
+
+    //Obtener turnos segun el cliente
+    getTurnoByCliente(): Turno[] | Turno | undefined{
+        throw new Error('Implemtar funcion')
+    }
+
+    //Crear Turno
+    addTurno(turnoNew:Turno): Turno{
+        throw new Error('Implemtar funcion')
+    }
+
+    //Cancelar Turno
+    deleteTurno(id:number): boolean{
+        throw new Error('Implemtar funcion')
+    }
+}
