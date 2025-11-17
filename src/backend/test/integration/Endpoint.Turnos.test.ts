bf1f52c658d79b524503beccb9ddf776003f0838
MatAviez05 <baezavilamateo@gmail.com>
2025-11-17 11:43:07 -0300

    test integration: Endpoint getAllTurnos

--- src/backend/test/integration/Endpoint.Turnos.test.ts
+++ src/backend/test/integration/Endpoint.Turnos.test.ts
@@ -20,4 +20,27 @@ describe('Porbar los distintos endpoints de la ruta Turnos', () => {
         expect(res.status).toBe(201)
         expect(res.body).toHaveProperty('cliente')
     })
+
+    it('Endpoint: getAllTurnos', async () => {
+        const res = await request(app)
+        .post('/api/turnos')
+        .send([{
+            cliente: 'Perez',
+            barbero: 'Agustin',
+            fecha: '10/10/10',
+            servicios: 'Corte',
+            tipo: 'Simple'
+        }, {
+            cliente: 'Checho',
+            barbero: 'Pedro',
+            fecha: '12/10/19',
+            servicios: 'Corte',
+            tipo: 'Combo'
+        }])
+        const get = await request(app)
+        .get('/api/turnos')
+
+        expect(get.status).toBe(200)
+        expect(get.body[0].barbero).toBe('Agustin')
+    })
 })
