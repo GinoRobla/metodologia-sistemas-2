81cf294fa502669ee270350dfe249281da56270a
MatAviez05 <baezavilamateo@gmail.com>
2025-11-17 20:31:07 -0300

    Endpoints de la ruta usuarios implementados

--- src/backend/src/controllers/usuarios.controller.ts
+++ src/backend/src/controllers/usuarios.controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+import { UsuarioService } from "../service/UsuarioService";
+
+const svc = new UsuarioService()
 
 export class UsuariosController {
     
@@ -7,19 +10,50 @@ export class UsuariosController {
     }
 
     public async getAllBarberos(req:Request, res:Response) {
-        
+        try{
+            const usuarios = svc.getAllBarberos()
+            if(usuarios.length === 0){
+                return res.status(404).json({error: 'No hay barberos registrados'})
+            }
+            return res.status(200).json(usuarios)
+        }catch{
+            return res.status(401).json({error: 'Error al obtener los datos'})
+        }
     }
 
     public async getAllClientes(req:Request, res:Response){
-
+        try{
+            const usuarios = svc.getAllClientes()
+            if(usuarios.length === 0){
+                return res.status(404).json({error: 'No hay clientes registrados'})
+            }
+            return res.status(200).json(usuarios)
+        }catch{
+            return res.status(401).json({error: 'Error al obtener los datos'})
+        }
     }
 
     public async addUsuario(req:Request, res:Response){
-
+        try{
+            const {nombre, email, telefono, contraseña, tipoUsuario} = req.body
+            const usuario = svc.addUsuario(nombre, email, telefono, contraseña, tipoUsuario)
+            return res.status(201).json(usuario)
+        }catch{
+            return res.status(401).json({error: 'Error al obtener los datos'})
+        }
     }
 
     public async deleteUsuario(req:Request, res:Response){
-
+        try{
+            const { email } = req.body
+            const success = svc.deleteUsuario(email)
+            if(!success){
+                return res.status(404).json({error: 'No se encontro ningun turno con ese nombre de cliente'})
+            }
+            return res.status(204).json({})
+        }catch{
+            return res.status(401).json({error: 'Error al obtener los datos'})
+        }
     }
 }
 
