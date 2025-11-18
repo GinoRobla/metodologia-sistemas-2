5b4bb751518c65bfa55080d7c03410909d65dfc9
MatAviez05 <baezavilamateo@gmail.com>
2025-11-17 23:59:01 -0300

    implementacion defuncion para verificar token y asignacion en las rutas

--- src/backend/src/routes/turnos.route.ts
+++ src/backend/src/routes/turnos.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import TurnosController from "../controllers/turnos.controller";
+import verifyTokenMiddleware from "../middlewares/verifyToken.middleware";
 
 export const routerTurnos = Router()
 
 routerTurnos.get('/', TurnosController.getAllTurnos)
 routerTurnos.get('/mis-turnos', TurnosController.getTurnoByCliente)
-routerTurnos.post('/', TurnosController.addTurno)
+routerTurnos.post('/', verifyTokenMiddleware, TurnosController.addTurno)
 //Por ahora el cancelar turnos sera por el nombre del cliente hasta que se concete con la base de datos
-routerTurnos.delete('/:id', TurnosController.deleteTurno)
+routerTurnos.delete('/:id', verifyTokenMiddleware, TurnosController.deleteTurno)
 
 export default routerTurnos
--- src/backend/src/routes/usuarios.route.ts
+++ src/backend/src/routes/usuarios.route.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import UsuariosController from "../controllers/usuarios.controller";
+import verifyTokenMiddleware from "../middlewares/verifyToken.middleware";
 
 export const routerUsuarios = Router()
 //Authorization
 routerUsuarios.post('/auth/register', UsuariosController.addUsuario)
 routerUsuarios.post('/auth/login', UsuariosController.login)
 //Barberos
-routerUsuarios.get('/list-barberos', UsuariosController.getAllBarberos)
+routerUsuarios.get('/list-barberos', verifyTokenMiddleware ,UsuariosController.getAllBarberos)
 //Clientes
-routerUsuarios.get('/list-clientes', UsuariosController.getAllClientes)
+routerUsuarios.get('/list-clientes', verifyTokenMiddleware ,UsuariosController.getAllClientes)
 //Deleteusuario
-routerUsuarios.delete('/:id', UsuariosController.deleteUsuario)
+routerUsuarios.delete('/:id', verifyTokenMiddleware, UsuariosController.deleteUsuario)
 
 export default routerUsuarios
