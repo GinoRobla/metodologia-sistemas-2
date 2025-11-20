f49e5f903c1c122c349cf5b78f044868b1e071bd
MatAviez05 <baezavilamateo@gmail.com>
2025-11-20 11:52:09 -0300

    Turno y counter modelos para mongodb

--- src/backend/src/models/Usuario.ts
+++
@@ -1,9 +0,0 @@
-export class Usuario {
-    constructor(
-        public nombre:string,
-        public email:string,
-        public telefono:string,
-        public contraseña:string,
-        public tipoUsuario:string, //Cliente | Barbero
-    ){}
-}
